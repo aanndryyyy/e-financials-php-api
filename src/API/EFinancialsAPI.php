35ce354334d8e7d159b32fa256f7251c6cede4e3
Andry Pedak <andry.pedak@gmail.com>
2023-02-06 17:41:54 +0000

    Init API file

---
+++ src/API/EFinancialsAPI.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EFinancialsClient\API;
+
+use GuzzleHttp\Client;
+
+class EFinancialsAPI {
+
+  public function __construct(
+    private ?Client $guzzle       = null,
+    private string  $apiKeyId     = '',
+    private string  $apiKeyPublic = '',
+    private string  $password     = '',
+    private string  $apiUrl       = "https://demo-rmp-api.rik.ee"
+  )
+  {
+
+    // TODO: Authentication.
+  }
+}
