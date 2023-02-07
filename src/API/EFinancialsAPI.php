5a36fcfcb30cf16135a78cf9b28442b5859ace68
Andry Pedak <andry.pedak@gmail.com>
2023-02-07 20:19:29 +0000

    PHPCS fix

--- src/API/EFinancialsAPI.php
+++ src/API/EFinancialsAPI.php
@@ -4,17 +4,16 @@ namespace EFinancialsClient\API;
 
 use GuzzleHttp\Client;
 
-class EFinancialsAPI {
+class EFinancialsAPI
+{
+    public function __construct(
+        private ?Client $guzzle = null,
+        private string $apiKeyId = '',
+        private string $apiKeyPublic = '',
+        private string $password = '',
+        private string $apiUrl = "https://demo-rmp-api.rik.ee"
+    ) {
 
-  public function __construct(
-    private ?Client $guzzle       = null,
-    private string  $apiKeyId     = '',
-    private string  $apiKeyPublic = '',
-    private string  $password     = '',
-    private string  $apiUrl       = "https://demo-rmp-api.rik.ee"
-  )
-  {
-
-    // TODO: Authentication.
-  }
+        // TODO: Authentication.
+    }
 }
