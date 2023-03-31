9fdb79ef2a9892a4662a580eafaa8ff4a75c8c64
Andry Pedak <andry.pedak@gmail.com>
2023-03-31 10:19:26 +0000

    Get all products endpoint

--- src/API/Products.php
+++ src/API/Products.php
@@ -2,8 +2,41 @@
 
 namespace EFinancialsClient\API;
 
+use DateTime;
+
 class Products extends AbstractAPI
 {
+    /**
+     * Retrieve the product list of the specified company.
+     *
+     * @see https://rmp-api.rik.ee/api.html#operation/get-products
+     *
+     * @param int             $page Page of responses to return.
+     * @param DateTime|string $modifiedSince Return only objects modified since provided timestamp.
+     *
+     * @return mixed
+     */
+    public function all(int $page = 1, DateTime|string $modifiedSince = ''): mixed
+    {
+        $query = [];
+
+        if ( $page !== 1 ) {
+            $query['page'] = $page;
+        }
+
+        if ( $modifiedSince !== '' ) {
+            // If $modifiedSince is a DateTime object, format it as an Atom string
+            // Otherwise, assign keep it as date string.
+            $query['modified_since'] = ($modifiedSince instanceof DateTime)
+                ? $modifiedSince -> format( \DateTimeInterface::ATOM )
+                : $modifiedSince;
+        }
+
+        $response = $this->client->request( 'GET', 'products', $query );
+
+        return $response;
+    }
+
     /**
      * Get a product.
      *
